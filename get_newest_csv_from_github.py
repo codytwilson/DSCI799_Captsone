c223ac32940ec04386dc8ca0b3db6c249998ccef
codytwilson <74381477+codytwilson@users.noreply.github.com>
2023-09-18 10:38:38 -0500

    add one off code to get historical fablisting

--- get_newest_csv_from_github.py
+++ get_newest_csv_from_github.py
@@ -16,7 +16,7 @@ repository_url = 'https://github.com/cwilsonCrystalSteel/Python'
 
 
 
-files = ['archive_FED.csv','Archive_CSM.csv','archive_CSF.csv','production_worksheet.csv']
+files = ['archive_FED.csv','Archive_CSM.csv','archive_CSF.csv','production_worksheet.csv','CSF_fablisting_2023.csv','CSM_fablisting_2023.csv','FED_fablisting_2023.csv']
 
 for file in files:
     
@@ -49,3 +49,39 @@ for file in files:
         print(f'File downloaded and saved as {local_file_path}')
     else:
         print(f'Failed to download the file. HTTP Status Code: {response.status_code}')
+
+
+
+# # only need to do this once
+# for year in [2022,2021,2020]:
+#     for shop in ['CSM','CSF','FED']:
+#         file = shop + ' ' + str(year) + '.csv'
+    
+        
+#         github_file_path = 'Capstone/' + file
+        
+#         # Construct the raw file URL
+#         raw_url = f'{repository_url}/raw/main/{github_file_path}'
+        
+#         # Make an HTTP GET request to download the file
+#         response = requests.get(raw_url)
+        
+#         # Check if the request was successful (HTTP status code 200)
+#         if response.status_code == 200:
+#             # Specify the local file path where you want to save the downloaded file
+#             local_dir = ".\\data\\"
+#             local_file_path = local_dir + file
+            
+#             # rename the old file if it exists so that the new one doesnt overwrite
+#             if os.path.exists(local_file_path):
+#                 new_file_name = local_dir + now_str + file
+#                 os.rename(local_file_path, new_file_name)
+                
+                
+#             # Write the content of the response to the local file
+#             with open(local_file_path, 'wb') as newfile:
+#                 newfile.write(response.content)
+                    
+#             print(f'File downloaded and saved as {local_file_path}')
+#         else:
+#             print(f'Failed to download the file. HTTP Status Code: {response.status_code}')
